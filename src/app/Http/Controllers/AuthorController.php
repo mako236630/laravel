5a02f1bce2792fcb52ab43775570df6b34585da2
mako <immako.236630@gmail.com>
2025-12-01 14:01:26 +0900

    CRUD処理:データの削除

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -2,39 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Author;
 use Illuminate\Http\Request;
+use App\Models\Author;
 
 class AuthorController extends Controller
 {
+    // データ一覧ページの表示
     public function index()
     {
         $authors = Author::all();
         return view('index', ['authors' => $authors]);
     }
+
+    // データ追加用ページの表示
     public function add()
     {
         return view('add');
     }
+
+    // 追加機能
     public function create(Request $request)
     {
         $form = $request->all();
         Author::create($form);
         return redirect('/');
     }
-
-    public function edit($id)
+    // データ編集ページの表示
+    public function edit(Request $request)
     {
-
-        $author = Author::find($id);
-
-        if (is_null($author)) {
-            return redirect('/'); // 一覧画面など安全な場所に戻す
-        }
-
+        $author = Author::find($request->id);
         return view('edit', ['form' => $author]);
     }
 
+    // 更新機能
     public function update(Request $request)
     {
         $form = $request->all();
@@ -43,5 +43,15 @@ class AuthorController extends Controller
         return redirect('/');
     }
 
+    public function delete(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('delete', ['author' => $author]);
+    }
 
+    public function remove(Request $request)
+    {
+        Author::find($request->id)->delete();
+        return redirect('/');
+    }
 }
--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -29,46 +29,44 @@
 <form action="/edit" method="POST">
     <table>
         @csrf
-        @method('patch')
-</form>
-<tr>
-    <th>
-        id
-    </th>
-    <td>
-        <input type="text" name="id" value="{{$form->id}}">
-    </td>
-</tr>
-<tr>
-    <th>
-        name
-    </th>
-    <td>
-        <input type="text" name="name" value="{{$form->name}}">
-    </td>
-</tr>
-<tr>
-    <th>
-        age
-    </th>
-    <td>
-        <input type="text" name="age" value="{{$form->age}}">
-    </td>
-</tr>
-<tr>
-    <th>
-        nationality
-    </th>
-    <td>
-        <input type="text" name="nationality" value="{{$form->nationality}}">
-    </td>
-</tr>
-<tr>
-    <th></th>
-    <td>
-        <button>送信</button>
-    </td>
-</tr>
-</table>
+        <tr>
+            <th>
+                id
+            </th>
+            <td>
+                <input type="text" name="id" value="{{$form->id}}">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                name
+            </th>
+            <td>
+                <input type="text" name="name" value="{{$form->name}}">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                age
+            </th>
+            <td>
+                <input type="text" name="age" value="{{$form->age}}">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                nationality
+            </th>
+            <td>
+                <input type="text" name="nationality" value="{{$form->nationality}}">
+            </td>
+        </tr>
+        <tr>
+            <th></th>
+            <td>
+                <button>送信</button>
+            </td>
+        </tr>
+    </table>
 </form>
 @endsection
