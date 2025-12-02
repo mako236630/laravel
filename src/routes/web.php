0395f50f9d2be991cb7f465935d83a3609c56e55
mako <immako.236630@gmail.com>
2025-12-02 13:23:40 +0900

    Revert "検索とモデル結合ルート" "デバッグ" This reverts commit d7e4b290db60f9b5171d9b0ea91b5eb1247e0b4d.

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 
 Route::get('/', [AuthorController::class, 'index']);
-Route::get('/add', [AuthorController::class, 'add']);
-Route::post('/add', [AuthorController::class, 'create']);
-Route::get('/edit/{author}', [AuthorController::class, 'edit']);
-Route::post('/edit', [AuthorController::class, 'update']);
-Route::get('/delete/{author}', [AuthorController::class, 'delete']);
-Route::post('/delete', [AuthorController::class, 'remove']);
 Route::get('/find', [AuthorController::class, 'find']);
 Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/author/{author}', [AuthorController::class, 'bind']);
