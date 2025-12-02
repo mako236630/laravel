fea9b191417094e26e0f2b264dbd9b1f869ab838
mako <immako.236630@gmail.com>
2025-12-02 09:57:25 +0900

    2章laravel,応用

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -2,56 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Author;
+use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    // データ一覧ページの表示
     public function index()
     {
         $authors = Author::all();
         return view('index', ['authors' => $authors]);
     }
-
-    // データ追加用ページの表示
-    public function add()
+    public function find()
     {
-        return view('add');
+        return view('find', ['input' => '']);
     }
-
-    // 追加機能
-    public function create(Request $request)
+    public function search(Request $request)
     {
-        $form = $request->all();
-        Author::create($form);
-        return redirect('/');
+        $item = Author::where('name', 'LIKE',"%{$request->input}%")->first();
+        $param = [
+            'input' => $request->input,
+            'item' => $item
+        ];
+        return view('find', $param);
     }
-    // データ編集ページの表示
-    public function edit(Request $request)
+    public function bind(Author $author)
     {
-        $author = Author::find($request->id);
-        return view('edit', ['form' => $author]);
-    }
-
-    // 更新機能
-    public function update(Request $request)
-    {
-        $form = $request->all();
-        unset($form['_token']);
-        Author::find($request->id)->update($form);
-        return redirect('/');
-    }
-
-    public function delete(Request $request)
-    {
-        $author = Author::find($request->id);
-        return view('delete', ['author' => $author]);
-    }
-
-    public function remove(Request $request)
-    {
-        Author::find($request->id)->delete();
-        return redirect('/');
+        $data = [
+            'item'=>$author,
+        ];
+        return view('author.binds', $data);
     }
 }
